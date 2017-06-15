43a2651f67514766fb33bd731509d883c1f19244
zhaoyim <yyyiming@163.com>
2017-06-15 11:37:25 +0800

    service instance delete

--- src/com/asiainfo/ocmanager/persistence/mapper/ServiceInstanceMapper.java
+++ src/com/asiainfo/ocmanager/persistence/mapper/ServiceInstanceMapper.java
@@ -1,16 +1,32 @@
 package com.asiainfo.ocmanager.persistence.mapper;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
 import com.asiainfo.ocmanager.persistence.model.ServiceInstance;
+
 /**
  * 
  * @author zhaoyim
  *
  */
 public interface ServiceInstanceMapper {
+	
+	
+	public List<ServiceInstance> selectServiceInstancesByTenant(@Param("tenantId") String tenantId);
+	
 	/**
 	 * 
 	 * @param serviceInstance
 	 */
 	public void insertServiceInstance(ServiceInstance serviceInstance);
+
+	/**
+	 * 
+	 * @param tenantId
+	 * @param instanceName
+	 */
+	public void deleteServiceInstance(@Param("tenantId") String tenantId, @Param("instanceName") String instanceName);
 
 }
--- src/com/asiainfo/ocmanager/persistence/test/TestServiceInstance.java
+++ src/com/asiainfo/ocmanager/persistence/test/TestServiceInstance.java
@@ -1,8 +1,11 @@
 package com.asiainfo.ocmanager.persistence.test;
 
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
 
 import com.asiainfo.ocmanager.persistence.mapper.ServiceInstanceMapper;
+import com.asiainfo.ocmanager.persistence.model.Role;
 import com.asiainfo.ocmanager.persistence.model.ServiceInstance;
 
 public class TestServiceInstance {
@@ -11,8 +14,19 @@ public class TestServiceInstance {
 		SqlSession session = DBConnectorFactory.getSession();
 		try {
 			ServiceInstanceMapper mapper = session.getMapper(ServiceInstanceMapper.class);
-			mapper.insertServiceInstance(new ServiceInstance("5", "inst5", "2", "300", "ETCD", "hdfs tenant 005 quota"));
+			mapper.insertServiceInstance(new ServiceInstance("9", "inst9", "2", "300", "ETCD", "hdfs tenant 005 quota"));
+			mapper.deleteServiceInstance("2", "inst9");
+			System.out.println("=== delete successfully ==");
+			
+			List<ServiceInstance> sis = mapper.selectServiceInstancesByTenant("2");
 
+			for (ServiceInstance si : sis) {
+				System.out.println(si.getId());
+				System.out.println(si.getServiceTypeName());
+
+			}
+			
+			
 			session.commit();
 		} catch (Exception e) {
 			session.rollback();
