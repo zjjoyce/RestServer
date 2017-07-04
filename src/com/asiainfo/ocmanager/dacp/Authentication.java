7f0967e3e7b92810dd4e319429aa5a3db22f3187
zhangfq7 <15110876490@163.com>
2017-07-04 09:38:32 +0800

    update

--- src/com/asiainfo/ocmanager/dacp/Authentication.java
+++ src/com/asiainfo/ocmanager/dacp/Authentication.java
@@ -2,6 +2,9 @@ package com.asiainfo.ocmanager.dacp;
 
 import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.SSLSession;
+import javax.net.ssl.*;
+import java.security.cert.X509Certificate;
+import java.security.cert.CertificateException;
 
 /**
  * Created by Allen on 2017/7/4.
@@ -11,48 +14,38 @@ public class Authentication {
 
     public static HostnameVerifier hv = new HostnameVerifier() {
         public boolean verify(String urlHostName, SSLSession session) {
-            System.out.println("Warning: URL Host: " + urlHostName + " vs. "
-                    + session.getPeerHost());
+            System.out.println("Warning: URL Host: " + urlHostName + " vs. " + session.getPeerHost());
             return true;
         }
     };
 
     public static void trustAllHttpsCertificates() throws Exception {
-        javax.net.ssl.TrustManager[] trustAllCerts = new javax.net.ssl.TrustManager[1];
-        javax.net.ssl.TrustManager tm = new miTM();
+        TrustManager[] trustAllCerts = new TrustManager[1];
+        TrustManager tm = new miTM();
         trustAllCerts[0] = tm;
-        javax.net.ssl.SSLContext sc = javax.net.ssl.SSLContext
-                .getInstance("SSL");
+        SSLContext sc = SSLContext.getInstance("SSL");
         sc.init(null, trustAllCerts, null);
-        javax.net.ssl.HttpsURLConnection.setDefaultSSLSocketFactory(sc
-                .getSocketFactory());
+        HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
     }
 
-    static class miTM implements javax.net.ssl.TrustManager,
-            javax.net.ssl.X509TrustManager {
-        public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+    static class miTM implements TrustManager, X509TrustManager {
+        public X509Certificate[] getAcceptedIssuers() {
             return null;
         }
 
-        public boolean isServerTrusted(
-                java.security.cert.X509Certificate[] certs) {
+        public boolean isServerTrusted(X509Certificate[] certs) {
             return true;
         }
 
-        public boolean isClientTrusted(
-                java.security.cert.X509Certificate[] certs) {
+        public boolean isClientTrusted(X509Certificate[] certs) {
             return true;
         }
 
-        public void checkServerTrusted(
-                java.security.cert.X509Certificate[] certs, String authType)
-                throws java.security.cert.CertificateException {
+        public void checkServerTrusted(X509Certificate[] certs, String authType) throws CertificateException {
             return;
         }
 
-        public void checkClientTrusted(
-                java.security.cert.X509Certificate[] certs, String authType)
-                throws java.security.cert.CertificateException {
+        public void checkClientTrusted(X509Certificate[] certs, String authType) throws CertificateException {
             return;
         }
     }
