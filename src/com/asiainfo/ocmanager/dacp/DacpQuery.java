293fecb5d5b99279476df05d0a078d9e6d2f122f
zhangfq7 <15110876490@163.com>
2017-07-04 09:46:17 +0800

    update

--- src/com/asiainfo/ocmanager/dacp/DacpQuery.java
+++ src/com/asiainfo/ocmanager/dacp/DacpQuery.java
@@ -15,53 +15,6 @@ public class DacpQuery {
     private static HttpsURLConnection conn;
     private static BufferedReader reader;
 
-    /*private static HostnameVerifier hv = new HostnameVerifier() {
-        public boolean verify(String urlHostName, SSLSession session) {
-            System.out.println("Warning: URL Host: " + urlHostName + " vs. "
-                    + session.getPeerHost());
-            return true;
-        }
-    };
-
-    private static void trustAllHttpsCertificates() throws Exception {
-        javax.net.ssl.TrustManager[] trustAllCerts = new javax.net.ssl.TrustManager[1];
-        javax.net.ssl.TrustManager tm = new miTM();
-        trustAllCerts[0] = tm;
-        javax.net.ssl.SSLContext sc = javax.net.ssl.SSLContext
-                .getInstance("SSL");
-        sc.init(null, trustAllCerts, null);
-        javax.net.ssl.HttpsURLConnection.setDefaultSSLSocketFactory(sc
-                .getSocketFactory());
-    }
-
-    static class miTM implements javax.net.ssl.TrustManager,
-            javax.net.ssl.X509TrustManager {
-        public java.security.cert.X509Certificate[] getAcceptedIssuers() {
-            return null;
-        }
-
-        public boolean isServerTrusted(
-                java.security.cert.X509Certificate[] certs) {
-            return true;
-        }
-
-        public boolean isClientTrusted(
-                java.security.cert.X509Certificate[] certs) {
-            return true;
-        }
-
-        public void checkServerTrusted(
-                java.security.cert.X509Certificate[] certs, String authType)
-                throws java.security.cert.CertificateException {
-            return;
-        }
-
-        public void checkClientTrusted(
-                java.security.cert.X509Certificate[] certs, String authType)
-                throws java.security.cert.CertificateException {
-            return;
-        }
-    }*/
     public static String GetData(String name)throws Exception {
 
         String url = Property.getDFProperties().get(Property.DATAFOUNDRY_URL_DACP);
@@ -76,8 +29,8 @@ public class DacpQuery {
 
         String restresult = "";
         Authentication.trustAllHttpsCertificates();
-        conn.setDefaultHostnameVerifier(Authentication.hv);
         conn = (HttpsURLConnection) new URL(dfRestUrl).openConnection();
+        conn.setDefaultHostnameVerifier(Authentication.hv);
         conn.setRequestMethod("GET");
         conn.setRequestProperty("Content-Type", "application/json");
         conn.setRequestProperty("Authorization","bearer " + token);
