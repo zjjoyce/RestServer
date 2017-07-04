cb0022706330851b5601e9006ebe814b3e453eac
zhangfq7 <15110876490@163.com>
2017-07-04 11:39:35 +0800

    update

--- src/com/asiainfo/ocmanager/dacp/DacpQuery.java
+++ src/com/asiainfo/ocmanager/dacp/DacpQuery.java
@@ -1,9 +1,13 @@
 package com.asiainfo.ocmanager.dacp;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.HttpsURLConnection;
 import javax.net.ssl.SSLSession;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 /**
@@ -11,34 +15,38 @@ import java.net.URL;
  * 调用DataFoundry API获取数据
  */
 public class DacpQuery {
-
+    private static Log logger = LogFactory.getLog(DacpQuery.class);
     private static HttpsURLConnection conn;
     private static BufferedReader reader;
 
-    public static String GetData(String name)throws Exception {
-
-        String url = Property.getDFProperties().get(Property.DATAFOUNDRY_URL_DACP);
-        String token = Property.getDFProperties().get(Property.DATAFOUNDRY_TOKEN_DACP);
-        String dfRestUrl = url + "/oapi/v1/namespaces/"+name+"/backingserviceinstances";
-        /*System.out.println(url);
-        System.out.println(token);
-        System.out.println(dfRestUrl);*/
-        /*HttpPost httpPost = new HttpPost(dfRestUrl);
-        httpPost.addHeader("Content-type", "application/json");
-        httpPost.addHeader("Authorization", "bearer " + token);*/
+    public static String GetData(String name){
 
         String restresult = "";
-        Authentication.trustAllHttpsCertificates();
-        conn = (HttpsURLConnection) new URL(dfRestUrl).openConnection();
-        conn.setDefaultHostnameVerifier(Authentication.hv);
-        conn.setRequestMethod("GET");
-        conn.setRequestProperty("Content-Type", "application/json");
-        conn.setRequestProperty("Authorization","bearer " + token);
-        reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-        String line = "";
-        while ((line = reader.readLine()) != null) {
-            //System.out.println(line);
-            restresult += line;
+        try{
+            String url = Property.getDFProperties().get(Property.DATAFOUNDRY_URL_DACP);
+            String token = Property.getDFProperties().get(Property.DATAFOUNDRY_TOKEN_DACP);
+            String dfRestUrl = url + "/oapi/v1/namespaces/"+name+"/backingserviceinstances";
+            /*System.out.println(url);
+            System.out.println(token);
+            System.out.println(dfRestUrl);*/
+            /*HttpPost httpPost = new HttpPost(dfRestUrl);
+            httpPost.addHeader("Content-type", "application/json");
+            httpPost.addHeader("Authorization", "bearer " + token);*/
+            Authentication.trustAllHttpsCertificates();
+            conn = (HttpsURLConnection) new URL(dfRestUrl).openConnection();
+            conn.setDefaultHostnameVerifier(Authentication.hv);
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Content-Type", "application/json");
+            conn.setRequestProperty("Authorization","bearer " + token);
+            reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+                //System.out.println(line);
+                restresult += line;
+            }
+        }catch (Exception e){
+            logger.error("IOException :" +e);
+            e.printStackTrace();
         }
 
         return restresult;
