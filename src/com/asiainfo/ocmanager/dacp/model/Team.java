f856293dc63db64ebdf58977fdbfa365278829da
于晶 <yujing2@asiainfo.com>
2017-07-07 12:41:05 +0800

    update team member name with _

--- src/com/asiainfo/ocmanager/dacp/model/Team.java
+++ src/com/asiainfo/ocmanager/dacp/model/Team.java
@@ -4,43 +4,43 @@ package com.asiainfo.ocmanager.dacp.model;
  * Created by yujin on 2017/7/3.
  */
 public class Team {
-    private String xmlId;
-    private int teamCode;
-    private String teamName;
-    private int teamType;
-    private String startDate;
+    private String xmlid;
+    private int team_code;
+    private String team_name;
+    private int team_type;
+    private String start_date;
     private String state;
-    private String iconPath;
+    private String icon_path;
     private String remark;
 
-    public Team(String xmlId,int teamCode,String teamName,int teamType,String startDate,String state,String iconPath){
-        this.xmlId = xmlId;
-        this.teamName = teamName;
-        this.teamCode = teamCode;
-        this.teamType = teamType;
-        this.startDate = startDate;
+    public Team(String xmlid,int team_code,String team_name,int team_type,String start_date,String state,String icon_path){
+        this.xmlid = xmlid;
+        this.team_name = team_name;
+        this.team_code = team_code;
+        this.team_type = team_type;
+        this.start_date = start_date;
         this.state = state;
-        this.iconPath = iconPath;
+        this.icon_path = icon_path;
     }
-    public void setXmlId(String xmlId){this.xmlId = xmlId;}
-    public String getXmlId(){return this.xmlId;}
+    public void setxmlid(String xmlid){this.xmlid = xmlid;}
+    public String getxmlid(){return this.xmlid;}
 
-    public void setTeamCode(int teamCode){this.teamCode = teamCode;}
-    public int getTeamCode(){return this.teamCode;}
-    public void setTeamName(String teamName){this.teamName = teamName;}
-    public String getTeamName(){return this.teamName;}
+    public void setteam_code(int team_code){this.team_code = team_code;}
+    public int getteam_code(){return this.team_code;}
+    public void setteam_name(String team_name){this.team_name = team_name;}
+    public String getteam_name(){return this.team_name;}
 
-    public void setTeamType(int teamType){this.teamType = teamType;}
-    public int getTeamType(){return this.teamType;}
+    public void setteam_type(int team_type){this.team_type = team_type;}
+    public int getteam_type(){return this.team_type;}
 
-    public void setStartDate(String startDate){this.startDate = startDate;}
-    public String getStartDate(){return this.startDate;}
+    public void setstart_date(String start_date){this.start_date = start_date;}
+    public String getstart_date(){return this.start_date;}
 
     public void setState(String state){this.state = state;}
     public String getState(){return this.state;}
 
-    public void setIconPath(String iconPath){this.iconPath = iconPath;}
-    public String getIconPath(){return this.iconPath;}
+    public void seticon_path(String icon_path){this.icon_path = icon_path;}
+    public String geticon_path(){return this.icon_path;}
 
     public void setRemark(String remark){this.remark = remark;}
     public String getRemark(){return this.remark;}
