f7b7945f55c13e61227efc4d9bc537954db0143a
zhangfq7 <15110876490@163.com>
2017-07-11 17:56:49 +0800

    commit

--- src/com/asiainfo/ocmanager/rest/resource/quotaUtils/quotaQuery.java
+++ src/com/asiainfo/ocmanager/rest/resource/quotaUtils/quotaQuery.java
@@ -4,7 +4,6 @@ import java.sql.*;
 import java.util.*;
 
 import com.asiainfo.ocmanager.persistence.model.Quota;
-import com.asiainfo.ocmanager.rest.resource.quotaUtils.HdfsUtils;
 import org.apache.hadoop.fs.Path;
 
 import com.mongodb.MongoClient;
@@ -106,8 +105,10 @@ public class quotaQuery {
         /*
         todo :move hive quota code to here
          */
-        HdfsUtils hdfsUtils = new HdfsUtils();
-        List<Quota> hdfsQuota = hdfsUtils.getHdfsQuota(new Path("/apps/hive/warehouse/"+dbname+".db"));
+        String path = "/apps/hive/warehouse/"+dbname+".db";
+        List<Quota> hdfsQuota = HdfsUtil.getHDFSData(path);
+//        HdfsUtils hdfsUtils = new HdfsUtils();
+//        List<Quota> hdfsQuota = hdfsUtils.getHdfsQuota(new Path("/apps/hive/warehouse/"+dbname+".db"));
         List<Quota> queueQuota = YarnUtil.getYarnData(queuename);
         //yarn quota
         Iterator<Quota> iterator = queueQuota.iterator();
