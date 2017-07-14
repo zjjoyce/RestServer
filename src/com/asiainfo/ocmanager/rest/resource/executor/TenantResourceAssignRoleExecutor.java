fd2c6a18958b55fc74703f1c57e6958c6d70736d
zhaoyim <yyyiming@163.com>
2017-07-14 19:34:33 +0800

    dacp

--- src/com/asiainfo/ocmanager/rest/resource/executor/TenantResourceAssignRoleExecutor.java
+++ src/com/asiainfo/ocmanager/rest/resource/executor/TenantResourceAssignRoleExecutor.java
@@ -13,6 +13,7 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import com.asiainfo.ocmanager.dacp.DacpAllResult;
 
 /**
  *
