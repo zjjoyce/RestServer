3c26e91609e44b22ac55b868e7109ccf5cd28fec
于晶 <yujing2@asiainfo.com>
2017-07-19 18:13:27 +0800

    add sh that caculate kafka quota

--- src/com/asiainfo/ocmanager/dacp/model/DacpResult.java
+++ src/com/asiainfo/ocmanager/dacp/model/DacpResult.java
@@ -5,14 +5,14 @@ package com.asiainfo.ocmanager.dacp.model;
  */
 public class DacpResult {
     private String result;
-    private String message;
-    public DacpResult(String result,String message){
+    private String msg;
+    public DacpResult(String result,String msg){
         this.result = result;
-        this.message = message;
+        this.msg = msg;
     }
     public void setResult(String result){this.result = result;}
     public String getResult(){return this.result;}
 
-    public void setMessage(String message){this.message = message;}
-    public String getMessage(){return this.message;}
+    public void setMessage(String msg){this.msg = msg;}
+    public String getMessage(){return this.msg;}
 }
