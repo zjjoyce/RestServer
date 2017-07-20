0d2ab19a598584913625a0042211a3d80ba5cdfb
闫林生 <yanls@asiainfo.com>
2017-07-20 16:34:29 +0800

    hadoop组件  failure 与 unbund不解析

--- src/com/asiainfo/ocmanager/dacp/dacpForResourceUtil.java
+++ src/com/asiainfo/ocmanager/dacp/dacpForResourceUtil.java
@@ -88,10 +88,9 @@ public class dacpForResourceUtil {
             mapInfo.put("database", dbRegisterList);
             mapInfo.put("transdatabase", dbDistributionList);
 
-        } catch (JsonIOException e) {
-            logger.info("DacpforResourceUtil JsonIOException " + e.getMessage());
-        } catch (JsonSyntaxException e) {
-            logger.info("DacpforResourceUtil JsonSyntaxException " + e.getMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+            logger.info("DacpforResourceUtil Exception " + e.getMessage());
         }
         return mapInfo;
     }
