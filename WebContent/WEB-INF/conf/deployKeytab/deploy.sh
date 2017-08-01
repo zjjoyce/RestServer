f265ebd84c958e58c81a3034633d919be1469687
于晶 <yujing2@asiainfo.com>
2017-08-01 16:56:13 +0800

    deploy keytab file to all dacp server;server.properties contain all servers with tag

--- WebContent/WEB-INF/conf/deployKeytab/deploy.sh
+++ WebContent/WEB-INF/conf/deployKeytab/deploy.sh
@@ -1,5 +1,6 @@
-# this shell can deploy src_file to dest_file
+# this shell can deploy src_file to dest_file to all server with tag
 # param including:src_file,dest_file,tag
+# author:yujing2
 set -x
 src_file=$1
 dest_file=$2
@@ -9,7 +10,7 @@ dir=`dirname $0`
 current=`cd $dir;pwd`
 servers=`cat $current/server.properties |grep -v "#"|grep "server"|awk -F ',' '{print $1}'`
 for i in $servers
-do 
+do
 	echo "scp $src_file to $i's dest_file"
 	scp $src_file $i:$dest_file
 done
