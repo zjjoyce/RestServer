9b5f183970773d218cd9283e59ab82c12805d23f
joyce <joycezj93@163.com>
2017-08-03 11:44:21 +0800

    keytabclient including create keytab file and base64 decode

--- src/com/asiainfo/ocmanager/dacp/utils/KeyTabClient.java
+++ src/com/asiainfo/ocmanager/dacp/utils/KeyTabClient.java
@@ -1,5 +1,7 @@
 package com.asiainfo.ocmanager.dacp.utils;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import sun.misc.BASE64Decoder;
 
 import java.io.BufferedWriter;
@@ -10,7 +12,8 @@ import java.io.IOException;
 
 public class KeyTabClient {
 
-    public static boolean transfer(String s,String dest){
+    public static final Log log = LogFactory.getLog(KeyTabClient.class);
+    public static boolean CreatKeyTab(String s,String dest){
         /**
          * @param dest
          * 解密后字符串存储文件
@@ -21,14 +24,18 @@ public class KeyTabClient {
             BufferedWriter out = new BufferedWriter(new FileWriter(dest));
                if(s!=null){
                    String decryptString = decryptBASE64(s);
-                   System.out.println("解密后：" + decryptString);
+                   log.info("解密后：" + decryptString);
                    out.write(decryptString);
+                   log.info("decryptString is true");
+                   return true;
             }
              out.close();
         }catch (FileNotFoundException e) {
-            System.out.println(" false");
+            log.info("error when read s" +e);
+            return false;
         } catch (IOException e){
-            System.out.println("false ");
+            log.info("error when read s,io is error" +e);
+            return false;
         }
         return true;
     }
@@ -45,7 +52,7 @@ public class KeyTabClient {
     public static void main(String[] args) {
         // write your code here
 
-        transfer("BQIAAAAqAAEACUNJVElDLkNPTQAGd2FuZ2JnAAAAAVl13cAAAAMACNn3wlJUOMvTAAAAQgABAAlDSVRJQy5DT00ABndhbmdiZwAAAAFZdd3AAAASACAwEY/T1lm2fNC3joARxQeE9pDa+6DnsC1bU1NS/dmXAwAAADoAAQAJQ0lUSUMuQ09NAAZ3YW5nYmcAAAABWXXdwAAAEAAYW8IxcGiwnrUQuo8THCBufH8cKlF/vLwaAAAAMgABAAlDSVRJQy5DT00ABndhbmdiZwAAAAFZdd3AAAAXABAEd9ZpaQc4mN6Hr8DCLvjXAAAAMgABAAlDSVRJQy5DT00ABndhbmdiZwAAAAFZdd3AAAARABCpJVdbAJEdbxCeowfiz1VL","d:\\toutput.txt");
+        CreatKeyTab("BQIAAAAqAAEACUNJVElDLkNPTQAGd2FuZ2JnAAAAAVl13cAAAAMACNn3wlJUOMvTAAAAQgABAAlDSVRJQy5DT00ABndhbmdiZwAAAAFZdd3AAAASACAwEY/T1lm2fNC3joARxQeE9pDa+6DnsC1bU1NS/dmXAwAAADoAAQAJQ0lUSUMuQ09NAAZ3YW5nYmcAAAABWXXdwAAAEAAYW8IxcGiwnrUQuo8THCBufH8cKlF/vLwaAAAAMgABAAlDSVRJQy5DT00ABndhbmdiZwAAAAFZdd3AAAAXABAEd9ZpaQc4mN6Hr8DCLvjXAAAAMgABAAlDSVRJQy5DT00ABndhbmdiZwAAAAFZdd3AAAARABCpJVdbAJEdbxCeowfiz1VL","d:\\toutput.txt");
     }
 
 }
